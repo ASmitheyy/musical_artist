f5c6f237bccecf29aa371b8ccda3bc36f39404f3
unknown <quinn.murphy@my.simpson.edu>
2018-11-01 17:39:21 -0500

    did it again but fixed

--- public_html/index.php
+++ public_html/index.php
@@ -12,7 +12,8 @@
 	<img src="img/header.png" alt="Lil SLeepy Header">
 </header>
 <h2 style="color:#572084;">Learn more about lil Sleepy</h2>
-<?php include 'navigation.php'; ?>
+  <?php include 'navigation.php'; ?>
+
 
  <?php include 'footer.php'; ?>
 </body>
